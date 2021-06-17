0196741dec26e2cbd06678bebb0c8dd2f1db608e
JApelgren <johan.apelgren@foreseeti.com>
2021-06-17 16:32:46 +0200

    cosmos_db

--- fetch_subscription_resources.py
+++ fetch_subscription_resources.py
@@ -94,6 +94,7 @@ from services import(
     connections,
     route_tables,
     storage_accounts,
+    cosmos_databases,
     sites,
     server_farms,
     namespaces,
@@ -256,6 +257,7 @@ def iterate_resources_to_json(
                 json_key = "routeTables"
 
             elif resource_type == "microsoft.storage/storageaccounts":
+                # object_to_add = storage_accounts.parse_obj(resource, resource_type, resource_group, sub_id, name, rg_client, rg_query_options, resource_id, DEBUGGING)
                 kind = resource.kind
 
                 str_query = f"resources | where type =~ 'microsoft.storage/storageaccounts' and name == '{name}'"
@@ -434,91 +436,7 @@ def iterate_resources_to_json(
                 json_key = "storageAccounts"
 
             elif resource_type == "microsoft.documentdb/databaseaccounts":
-                str_query = f"resources | where type =~ 'microsoft.documentdb/databaseaccounts' and name == '{name}'"
-                query = arg.models.QueryRequest(
-                    subscriptions=[sub_id], query=str_query, options=rg_query_options,
-                )
-                try:
-                    rg_results_as_dict = rg_client.resources(query=query).__dict__
-                except:
-                    if DEBUGGING:
-                        print(
-                            f"ERROR: Couldn't execute resource graph query of {name}, skipping asset."
-                        )
-                    continue
-                raw_properties = rg_results_as_dict["data"][0]["properties"]
-
-                virtual_network_rules = []
-                try:
-                    raw_network_rules = raw_properties["virtualNetworkRules"]
-                    for raw_nwr in raw_network_rules or []:
-                        nwr_id = raw_nwr.get("id")
-                        if nwr_id:
-                            virtual_network_rules.append(nwr_id)
-                except KeyError:
-                    pass
-
-                # If any networks can access the cosmosDB account or not
-                restricted_access = (
-                    False
-                    if raw_properties.get("isVirtualNetworkFilterEnabled") == False
-                    else True
-                )
-
-                # ipRangeFilter
-                try:
-                    raw_iprange = raw_properties["ipRangeFilter"]
-                    ip_range_filter = raw_iprange.split(",")
-                except:
-                    ip_range_filter = []
-
-                # disableKeyBasedMetadataWriteAccess
-                disable_key_based_metadata_write_access = (
-                    False
-                    if raw_properties.get("disableKeyBasedMetadataWriteAccess") == False
-                    else True
-                )
-
-                raw_api_types = raw_properties.get("EnabledApiTypes")
-                trimmed_api_types = str(raw_api_types).replace(" ", "")
-                api_types = trimmed_api_types.split(",")
-
-                # TODO: Try and find the actual databases, containers and so on.
-                """try:
-                    cosmos_client = CosmosDBManagementClient(
-                        credentials=credential, subscription_id=sub_id
-                    )
-                except:
-                    print(
-                        f"Could not initialise CosmosDBClient on {resourceId} to list databases and containers"
-                    )
-                try:
-                    sql_databases = cosmos_client.sql_resources.list_sql_databases(
-                        resource_group_name=resource_group, account_name=name
-                    )
-                    print(sql_databases)
-                    mylist = [x for x in sql_databases.next]
-                    print("hello: ", mylist)
-                    print(type(sql_databases))
-                    for x in sql_databases.next() or []:
-                        print(x)
-                except:
-                    print(
-                        f"Could not list sql databases in cosmosdb account {name} in resource_group {resource_group}"
-                    )"""
-
-                object_to_add = CosmosDB(
-                    resourceId=resource_id,
-                    name=name,
-                    resourceGroup=resource_group,
-                    provider=resource_type,
-                    restrictedAccess=restricted_access,
-                    virtualNetworkRules=virtual_network_rules,
-                    ipRangeFilter=ip_range_filter,
-                    apiTypes=api_types,
-                    disableKeyBasedMetadataWriteAccess=disable_key_based_metadata_write_access,
-                )
-
+                object_to_add = cosmos_databases.parse_obj(resource_type, resource_group, sub_id, name, rg_client, rg_query_options, resource_id, DEBUGGING)
                 json_key = "cosmosDBs"
 
             elif resource_type == "microsoft.web/sites":
