2e21d69a326f9f7aa11a894ec661fee12e3bb7dd
Gyllingen <andreas.gylling@foreseeti.com>
2021-08-24 09:33:51 +0200

    Fixed issues with vnet_gws and added documentation

--- services/vnet_gateways.py
+++ services/vnet_gateways.py
@@ -17,41 +17,53 @@ def parse_obj(resource_type, resource_group, sub_id, name, rg_client, rg_query_o
         return None
     raw_properties = rg_results_as_dict["data"][0]["properties"]
 
-    capacity = raw_properties["sku"]["capacity"]
-    raw_ip_configs = raw_properties["ipConfigurations"]
+    try:
+        capacity = raw_properties["sku"]["capacity"]
+    except KeyError:
+        if DEBUGGING:
+            print(
+                f"WARNING: Couldn't get ['sku']['capacity'] from virtual network gateway {name}, assuming value '1'"
+            )
+        capacity = 1
+    raw_ip_configs = raw_properties.get("ipConfigurations")
+    if not raw_ip_configs:
+        if DEBUGGING:
+            print(
+                f"WARNING: Couldn't get ipConfigurations of virtual network gateway {name}. Impact: potential missing model associations"
+            )
     ip_configs = []
     for raw_ip_config in raw_ip_configs or []:
         ip_config = {
-            "id": raw_ip_config["id"],
-            "name": raw_ip_config["name"],
-            "publicIpAddress": raw_ip_config["properties"][
-                "publicIPAddress"
-            ]["id"],
-            "subnet": raw_ip_config["properties"]["subnet"]["id"],
+            "id": raw_ip_config.get("id"),
+            "name": raw_ip_config.get("name"),
+            "publicIpAddress": raw_ip_config.get("properties", {}).get(
+                "publicIPAddress", {}).get("id"),
+            "subnet": raw_ip_config.get("properties",{}).get("subnet",{}).get("id"),
         }
         ip_configs.append(ip_config)
-    raw_bgp_settings = raw_properties["bgpSettings"]
+    raw_bgp_settings = raw_properties.get("bgpSettings")
+    if not raw_bgp_settings:
+        if DEBUGGING:
+            print(
+                f"WARNING: Couldn't get bgpSettings of virtual network gateway {name}. Impact: potential missing model associations"
+            )
     bgp_settings = []
     for raw_bgp_setting in (
-        raw_bgp_settings.get("bgpPeeringAddresses") or []
+        raw_bgp_settings.get("bgpPeeringAddresses", [])
     ):
         bgp_setting = {
-            "ipConfigId": raw_bgp_setting["ipconfigurationId"],
-            "tunnelIpAddress": raw_bgp_setting["tunnelIpAddresses"]
-            if raw_bgp_setting.get("tunnelIpAddresses")
-            else [],
-            "customBgpIpAddresses": raw_bgp_setting["customBgpIpAddresses"],
-            "defaultBgpIpAddresses": raw_bgp_setting[
-                "defaultBgpIpAddresses"
-            ],
+            "ipConfigId": raw_bgp_setting.get("ipconfigurationId"),
+            "tunnelIpAddress": raw_bgp_setting.get("tunnelIpAddresses", list()),
+            "customBgpIpAddresses": raw_bgp_setting.get("customBgpIpAddresses", list()),
+            "defaultBgpIpAddresses": raw_bgp_setting.get(
+                "defaultBgpIpAddresses", list()
+            ),
         }
         bgp_settings.append(bgp_setting)
     final_bgp_setting = {
         "bgpPeeringAddresses": bgp_settings,
-        "asn": raw_bgp_settings["asn"],
-        "bgpPeeringAddress": raw_bgp_settings["bgpPeeringAddress"]
-        if raw_bgp_settings.get("bgpPeeringAddress")
-        else None,
+        "asn": raw_bgp_settings.get("asn"),
+        "bgpPeeringAddress": raw_bgp_settings.get("bgpPeeringAddress")
     }
 
     object_to_add = VnetGateway(
